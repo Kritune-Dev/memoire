908c5ae98d4eef7b83aa3e2bb86155b700ca000b
Corentin Bacconnais <corentin.bacconnais@gmail.com>
2022-06-17 10:24:54 +0200

    🚧 🎨 : Transfert to react - Background Image Ok

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Confirmation from './pages/Confirmation'
+import Error from './components/Error'
+import BackgroundImage from './components/BackgroundImage'
+import { createGlobalStyle } from 'styled-components'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      windowWidth: 0,
+      windowHeight: 0,
+    }
+  }
+
+  componentDidMount() {
+    this.updateDimensions()
+    window.addEventListener('resize', this.updateDimensions.bind(this))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions.bind(this))
+  }
+
+  updateDimensions() {
+    let windowWidth = typeof window !== 'undefined' ? window.innerWidth : 0
+    let windowHeight = typeof window !== 'undefined' ? window.innerHeight : 0
+
+    this.setState({ windowWidth, windowHeight })
+  }
+
+  render() {
+    const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
+    }
+`
+
+    return (
+      <Router>
+        <GlobalStyle />
+        <BackgroundImage />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/confirmation" element={<Confirmation/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+      </Router>
+    )
+  }
+}
+
+export default App;
