ff2e00fc57556a566bd032b73a27574241da63b1
Kritune <corentin.bacconnais@gmail.com>
2022-07-26 18:14:25 +0200

    Test

--- next.config.js
+++ next.config.js
@@ -9,4 +9,19 @@ module.exports = {
   images: {
     domains: authorizedDomains.domains,
   },
+	
+	async redirects() {
+    return [
+      {
+        source: '/0',
+        destination: 'https://framaforms.org/la-recuperation-du-sportif-1651152805',
+        permanent: true
+      },
+      {
+        source: '/16',
+        destination: 'https://framaforms.org/complementarite-kine-osteopathe-et-prothese-de-hanche-1640434779',
+        permanent: true
+      }
+    ];
+  }
 }
